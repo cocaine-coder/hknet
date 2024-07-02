7a3297e528d9d8fe521c2c7e0ef054e35833936d
cocaine_coder <tracywang1997@outlook.com>
2024-07-02 17:34:00 +0800

    init

---
+++ test/HK.Net.Win32.Net8.Test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HK.Net;
+
+if (CHCNetSDK.NET_DVR_Init())
+    Console.WriteLine("sdk 初始化成功");
+else
+    Console.WriteLine("sdk 初始化失败");
---
+++ test/HK.Net.Win64.Framework.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HK.Net.Win64.Framework.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (CHCNetSDK.NET_DVR_Init())
+                Console.WriteLine("sdk 初始化成功");
+            else
+                Console.WriteLine("sdk 初始化失败");
+
+            Console.ReadKey();
+        }
+    }
+}
